8fd60720651d712a3fa9c04e30591c84fd6da81a
ReneeSlamka <renee.slamka@gmail.com>
2017-02-24 03:09:04 -0800

    Top navbar is done, only thing that may change is color scheme.

--- public/assets/js/mobile-menu-transition.js
+++ public/assets/js/mobile-menu-transition.js
@@ -1,17 +1,13 @@
 $(document).ready(function() {
     $("#mobile-menu-button").click(function(){
-        var $navbar = $(".site-links-section");
+        var $navbar = $("#topNavToggleBlock");
 
         if ($navbar.css("display") == "none") {
             $navbar.css({"display": "block"});
             $("#top-navbar-mobile-linebreak").css({"display": "block"});
-            $(".top-navbar-container").css({"height": "296px"});
-            $(".top-navbar-underimage").css({"height": "296px"});
         } else {
             $navbar.css({"display":"none"});
             $("#top-navbar-mobile-linebreak").css({"display": "none"});
-            $(".top-navbar-container").css({"height": "80px"});
-            $(".top-navbar-underimage").css({"height": "80px"});
         }
     });
 });
