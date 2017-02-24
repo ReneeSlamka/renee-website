7a82ad9d0529c45ed63467069f64b9433223a71c
ReneeSlamka <renee.slamka@gmail.com>
2017-02-24 15:55:02 -0800

    Isotope filter now working on project page. Need to fix alignment of panels and style buttons.

--- public/assets/js/isotope-project-grid.js
+++ public/assets/js/isotope-project-grid.js
@@ -1,5 +1,19 @@
-$('.grid').isotope({
-    // options
-    itemSelector: '.grid-item',
-    layoutMode: 'fitRows'
+$(document).ready(function() {
+    var $grid = $('.grid');
+    $grid.isotope({
+        // options
+        itemSelector: '.grid-item'//,
+        //layoutMode: 'fitRows'
+    });
+
+// filter items on button click
+    $('.filter-button-group').on( 'click', 'button', function() {
+        //var filterValue = $(this).attr('data-filter');
+        //$grid.isotope({ filter: filterValue });
+        var selector = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: selector
+        });
+        return false;
+    });
 });
